48b53ab789917873063f1afec68ccd6f84095c8e
NamrataP02 <namu877patil@gmail.com>
2024-03-26 03:40:38 +0530

    updated files

---
+++ javascriptCode/landing-page.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginButton").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("registerButton").addEventListener("click", function() {
+        window.location.href = "register-page.html";
+    });
+});
